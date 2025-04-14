c3e3692bf9ce6dc9e11ff037a31c65087641dae9
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-04-14 19:22:52 +0300

    replace manual constructor implementation with @AllArgsConstructor

--- src/main/java/ru/practicum/shareit/booking/dto/BookingShortDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingShortDto.java
@@ -1,24 +1,17 @@
 package ru.practicum.shareit.booking.dto;
 
 import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class BookingShortDto {
     private Long id;
     private Long bookerId;
     private Long itemId;
     private LocalDateTime start;
     private LocalDateTime end;
-
-    public BookingShortDto(Long id, Long bookerId, Long itemId, LocalDateTime start,
-        LocalDateTime end) {
-        this.id = id;
-        this.bookerId = bookerId;
-        this.itemId = itemId;
-        this.start = start;
-        this.end = end;
-    }
 }
