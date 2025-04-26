2e923970f2d092fd0a576d3c887f5858ca4858ad
Pepe Ronin <ivanpetrovskiy98@gmail.com>
2025-04-27 00:41:46 +0300

    initial attempt to move the app over to a server module

--- server/src/main/java/ru/practicum/shareit/user/dto/NewUserDto.java
+++ server/src/main/java/ru/practicum/shareit/user/dto/NewUserDto.java
@@ -4,9 +4,11 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class NewUserDto {
 
     @NotBlank(message = "Name cannot be blank")
